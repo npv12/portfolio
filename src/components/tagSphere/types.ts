ea843a2213bbddf5b15f711a381f4b447ecaddb5
Pranav <npv12@iitbbs.ac.in>
2023-03-06 20:11:47 +0530

    chore: improve tagsphere
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

---
+++ src/components/tagSphere/types.ts
@@ -0,0 +1,38 @@
+import { CSSProperties } from "react";
+
+export type tagSphereProps = {
+  skills: string[];
+  radius?: number;
+  maxSpeed: number;
+  initialSpeed: number;
+  initialDirection: 135;
+  keepRollingAfterMouseOut: boolean;
+  className?: string;
+  style?: CSSProperties;
+  useContainerInlineStyles: boolean;
+  fullWidth: boolean;
+  fullHeight: boolean;
+};
+
+export const defaultState: tagSphereProps = {
+  skills: ["python", "java", "C++", "C", "mongoDB", "go", "rust"],
+  maxSpeed: 4,
+  initialSpeed: 32,
+  initialDirection: 135,
+  keepRollingAfterMouseOut: false,
+  useContainerInlineStyles: true,
+  fullWidth: false,
+  fullHeight: false,
+};
+
+export const defaultStyles = {
+  getContainer: (radius: number, fullWidth: boolean, fullHeight: boolean) =>
+    ({
+      position: "relative",
+      width: fullWidth ? "100%" : `${2 * radius}px`,
+      maxWidth: "100%",
+      minHeight: `${2 * radius}px`,
+      height: fullHeight ? "100%" : `${2 * radius}px`,
+      touchAction: "none",
+    } as CSSProperties),
+};
---
+++ src/types/skills.ts
@@ -0,0 +1,42 @@
+export const languages = [
+  "JavaScript",
+  "TypeScript",
+  "Rust",
+  "C++",
+  "C",
+  "Kotlin",
+  "Go",
+  "Rust",
+];
+export const uiFrameworkOrComponents = [
+  "TailwindCSS",
+  "Styled Components",
+  "Material UI",
+  "Bootstrap",
+];
+export const frameworkOrLibraries = [
+  "Sass",
+  "ReactJs",
+  "NextJs",
+  "VueJs",
+  "Svelte",
+  "NodeJs",
+  "ExpressJs",
+];
+export const databases = ["Firebase", "MariaDB", "MongoDB", "MySQL"];
+export const others = [
+  "Figma",
+  "VsCode",
+  "Framer Motion",
+  "Vite",
+  "Linux",
+  "RHEL",
+];
+
+export const allSkills = [
+  ...languages,
+  ...uiFrameworkOrComponents,
+  ...frameworkOrLibraries,
+  ...databases,
+  ...others,
+];
