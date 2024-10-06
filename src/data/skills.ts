b05d53e0b19a4489a616c91e2ca724e5897de80c
Pranav <npv12@iitbbs.ac.in>
2024-10-07 04:35:40 +0530

    chore: add my basic skills together
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

---
+++ src/data/skills.ts
@@ -0,0 +1,33 @@
+export const languages = [
+  "JavaScript",
+  "TypeScript",
+  "Rust",
+  "C++",
+  "C",
+  "Kotlin",
+  "Go",
+  "Rust",
+];
+export const uiFrameworkOrComponents = [
+  "TailwindCSS",
+  "Material UI",
+  "Bootstrap",
+];
+export const frameworkOrLibraries = [
+  "Sass",
+  "React",
+  "NextJs",
+  "Vue",
+  "Svelte",
+  "NodeJs",
+];
+export const databases = ["Firebase", "MariaDB", "MongoDB", "MySQL"];
+export const others = ["Figma", "VsCode", "ViteJS", "Linux", "RHEL"];
+
+export const allSkills = [
+  ...languages,
+  ...uiFrameworkOrComponents,
+  ...frameworkOrLibraries,
+  ...databases,
+  ...others,
+];
