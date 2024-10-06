8cc0c3c14679695b3dfb0407e13e0487586f00f3
Pranav <npv12@iitbbs.ac.in>
2024-10-07 04:35:54 +0530

    feat: add TagSphere
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

---
+++ src/components/TagSphere/TagSphere.tsx
@@ -0,0 +1,184 @@
+import { createRef, useEffect, useRef, useState } from "react";
+
+import { createItem, fetchIconFromText, updateItemPosition } from "./helper";
+import { defaultState, defaultStyles, tagSphereProps } from "./types";
+
+export default function TagSphere(props: any) {
+  const {
+    maxSpeed,
+    initialSpeed,
+    skills,
+    initialDirection,
+    keepRollingAfterMouseOut,
+    fullHeight,
+    fullWidth,
+    style,
+    useContainerInlineStyles,
+  }: tagSphereProps = { ...defaultState, ...props };
+
+  let radius = props.radius;
+
+  if (!radius) {
+    radius = skills.length * 10;
+  }
+
+  const depth = 2 * radius;
+  const size = 1.5 * radius;
+  const itemHooks = skills.map(() => createRef());
+  const [items, setItems]: [any[], any] = useState([]);
+
+  useEffect(() => {
+    setItems(() =>
+      skills.map((skill, index) => {
+        const skillImg = (
+          <img
+            width={50}
+            height={50}
+            src={fetchIconFromText(skill)}
+            alt={"Random image"}
+          />
+        );
+        return createItem(
+          skillImg,
+          index,
+          skills.length,
+          size,
+          itemHooks[index]
+        );
+      })
+    );
+  }, [skills]);
+
+  const containerRef = useRef(null);
+  const [firstRender, setFirstRender] = useState(true);
+  const [lessSpeed, setLessSpeed] = useState(maxSpeed);
+  const [active, setActive] = useState(false);
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+
+  const handleMouseMove = (e: any) => {
+    // @ts-ignore
+    const rect = containerRef.current.getBoundingClientRect();
+
+    setMouseX(() => (e.clientX - (rect.left + rect.width / 2)) / 5);
+    setMouseY(() => (e.clientY - (rect.top + rect.height / 2)) / 5);
+  };
+
+  const checkTouchCoordinates = (e: any) => {
+    // @ts-ignore
+    const rect = containerRef.current.getBoundingClientRect();
+    const touchX = e.targetTouches[0].clientX;
+    const touchY = e.targetTouches[0].clientY;
+
+    if (
+      touchX > rect.left &&
+      touchX < rect.right &&
+      touchY < rect.bottom &&
+      touchY > rect.top
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const next = () => {
+    setItems((items: any) => {
+      if (lessSpeed == 0) return items;
+
+      let a, b;
+      if (!keepRollingAfterMouseOut && !active && !firstRender) {
+        setLessSpeed((lessSpeedCurrent) => {
+          const lessConstant = lessSpeed * (maxSpeed / 200);
+
+          return lessSpeedCurrent - lessConstant > 0.01
+            ? lessSpeedCurrent - lessConstant
+            : 0;
+        });
+
+        a = -(Math.min(Math.max(-mouseY, -size), size) / radius) * lessSpeed;
+        b = (Math.min(Math.max(-mouseX, -size), size) / radius) * lessSpeed;
+      } else if (!active && !firstRender && keepRollingAfterMouseOut) {
+        a =
+          -(Math.min(Math.max(-mouseY, -size), size) / radius) *
+          (maxSpeed * 0.5);
+        b =
+          (Math.min(Math.max(-mouseX, -size), size) / radius) *
+          (maxSpeed * 0.5);
+      } else {
+        a = -(Math.min(Math.max(-mouseY, -size), size) / radius) * maxSpeed;
+        b = (Math.min(Math.max(-mouseX, -size), size) / radius) * maxSpeed;
+      }
+
+      if (Math.abs(a) <= 0.01 && Math.abs(b) <= 0.01) return items; // pause
+
+      // calculate offset
+      const l = Math.PI / 180;
+      const sc = [
+        Math.sin(a * l),
+        Math.cos(a * l),
+        Math.sin(b * l),
+        Math.cos(b * l),
+      ];
+
+      return items.map((item: any) => updateItemPosition(item, sc, depth));
+    });
+  };
+
+  const init = () => {
+    setActive(false);
+    const mouseX0 = initialSpeed * Math.sin(initialDirection * (Math.PI / 180));
+    const mouseY0 =
+      -initialSpeed * Math.cos(initialDirection * (Math.PI / 180));
+
+    setMouseX(() => mouseX0);
+    setMouseY(() => mouseY0);
+
+    next();
+  };
+
+  useEffect(() => {
+    init();
+    setItems((items: any) => [...items]);
+  }, []);
+
+  useEffect(() => {
+    const animationFrame = requestAnimationFrame(next);
+    return () => cancelAnimationFrame(animationFrame);
+  }, [mouseX, mouseY, lessSpeed, active, items, props.radius]);
+
+  return (
+    <div
+      className={props.className}
+      ref={containerRef}
+      onMouseOver={() => {
+        setActive(() => true);
+        setFirstRender(() => false);
+        setLessSpeed(() => maxSpeed);
+      }}
+      onMouseOut={() => {
+        setActive(() => false);
+      }}
+      onMouseMove={handleMouseMove}
+      onTouchStart={() => {
+        setActive(true);
+        setLessSpeed(() => maxSpeed);
+        setFirstRender(() => false);
+      }}
+      onTouchMove={(e) => {
+        if (checkTouchCoordinates(e)) {
+          handleMouseMove(e.targetTouches[0]);
+        } else {
+          setActive(false);
+        }
+      }}
+      style={
+        useContainerInlineStyles
+          ? style || defaultStyles.getContainer(radius, fullWidth, fullHeight)
+          : undefined
+      }
+    >
+      {items.map((item) => item.el)}
+    </div>
+  );
+}
---
+++ src/components/TagSphere/helper.tsx
@@ -0,0 +1,126 @@
+import { CSSProperties, ReactNode } from "react";
+
+export const fetchIconFromText = (text: string) => {
+  /**
+   * Fetches all logos from https://cdn.svgporn.com
+   * Handle special cases like C++
+   */
+
+  const baseUrl = "https://cdn.svgporn.com/logos";
+
+  if (text.includes(" ")) {
+    text = text.replace(" ", "-");
+  }
+
+  switch (text.toLowerCase()) {
+    case "c++":
+      return `${baseUrl}/c-plusplus.svg`;
+    case "mongodb":
+      return `${baseUrl}/mongodb-icon.svg`;
+    case "rhel":
+      return `${baseUrl}/redhat-icon.svg`;
+    case "linux":
+      return `${baseUrl}/linux-tux.svg`;
+    case "vscode":
+      return `${baseUrl}/visual-studio-code.svg`;
+    case "svelte":
+      return `${baseUrl}/svelte-icon.svg`;
+    default:
+      return `${baseUrl}/${text.toLowerCase()}.svg`;
+  }
+};
+
+export const computeInitialPosition = (
+  index: number,
+  textsLength: number,
+  size: number
+) => {
+  const phi = Math.acos(-1 + (2 * index + 1) / textsLength);
+  const theta = Math.sqrt((textsLength + 1) * Math.PI) * phi;
+  return {
+    x: (size * Math.cos(theta) * Math.sin(phi)) / 2,
+    y: (size * Math.sin(theta) * Math.sin(phi)) / 2,
+    z: (size * Math.cos(phi)) / 2,
+  };
+};
+
+export const updateItemPosition = (item: any, sc: number[], depth: number) => {
+  const newItem = { ...item, scale: "" };
+  const rx1 = item.x;
+  const ry1 = item.y * sc[1] + item.z * -sc[0];
+  const rz1 = item.y * sc[0] + item.z * sc[1];
+
+  const rx2 = rx1 * sc[3] + rz1 * sc[2];
+  const ry2 = ry1;
+  const rz2 = rz1 * sc[3] - rx1 * sc[2];
+
+  const per = (2 * depth) / (2 * depth + rz2); // todo
+  newItem.x = rx2;
+  newItem.y = ry2;
+  newItem.z = rz2;
+
+  if (newItem.x === item.x && newItem.y === item.y && newItem.z === item.z) {
+    return item;
+  }
+
+  newItem.scale = per.toFixed(3);
+  let alpha: number = per * per - 0.25;
+  alpha = parseFloat((alpha > 1 ? 1 : alpha).toFixed(3));
+
+  const itemEl = newItem.ref.current;
+  const left = (newItem.x - itemEl.offsetWidth / 2).toFixed(2);
+  const top = (newItem.y - itemEl.offsetHeight / 2).toFixed(2);
+  const transform = `translate3d(${left}px, ${top}px, 0) scale(${newItem.scale})`;
+
+  itemEl.style.WebkitTransform = transform;
+  itemEl.style.MozTransform = transform;
+  itemEl.style.OTransform = transform;
+  itemEl.style.transform = transform;
+  itemEl.style.filter = `grayscale(${(alpha - 1) * -8}) blur(${
+    (alpha - 1) * -5 > 1 ? Math.floor((alpha - 1) * -8) : 0
+  }px)`;
+  itemEl.style.zIndex = Math.floor(alpha * 1000);
+  itemEl.style.opacity = alpha;
+
+  return newItem;
+};
+
+export const createItem = (
+  item: ReactNode,
+  index: number,
+  textsLength: number,
+  size: number,
+  itemRef: any
+) => {
+  const transformOrigin = "50% 50%";
+  const transform = "translate3d(-50%, -50%, 0) scale(1)";
+  const itemStyles = {
+    willChange: "transform, opacity, filter",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    zIndex: index + 1,
+    filter: "alpha(opacity=0)",
+    opacity: 0,
+    WebkitTransformOrigin: transformOrigin,
+    MozTransformOrigin: transformOrigin,
+    OTransformOrigin: transformOrigin,
+    transformOrigin: transformOrigin,
+    WebkitTransform: transform,
+    MozTransform: transform,
+    OTransform: transform,
+    transform: transform,
+  } as CSSProperties;
+  // @ts-ignore
+  const itemEl = (
+    <span ref={itemRef} key={index} style={itemStyles}>
+      {item}
+    </span>
+  );
+
+  return {
+    ref: itemRef,
+    el: itemEl,
+    ...computeInitialPosition(index, textsLength, size),
+  };
+};
