66aaa7004ac9f7ca9352ae9c25306d1c50837c39
Pranav <npv12@iitbbs.ac.in>
2024-10-26 18:01:56 +0530

    feat: add a divider to the title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,12 @@
 const Title = ({ title }: { title: string }) => {
   return (
-    <h1 className="text-5xl font-bold mb-6 bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent">
-      {title}
-    </h1>
-  );
+    <div className="flex items-center gap-4">
+      <h1 className="text-5xl font-bold bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent">
+        {title}
+      </h1>
+      <div className="h-[2px] w-64 bg-neutral-500"></div>
+    </div>  
+    );
 };
 
 export default Title;
